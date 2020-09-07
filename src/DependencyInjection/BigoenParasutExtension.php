72bac8cf1df5ae440a6c07b9a4cced22c7bdb175
Şafak Saylam <safaksaylamm@gmail.com>
2020-09-07 18:35:45 +0300

    Moved services from php to yaml.

--- src/DependencyInjection/BigoenParasutExtension.php
+++ src/DependencyInjection/BigoenParasutExtension.php
@@ -7,7 +7,7 @@ namespace Bigoen\ParasutBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * @author Şafak Saylam <safak@bigoen.com>
@@ -16,7 +16,7 @@ class BigoenParasutExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $loader = new PhpFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.php');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yaml');
     }
 }
